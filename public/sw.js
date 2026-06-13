0fab744f4596e1b3660db37d5a8d7751e81b4b18
Odin Evaristus <odinevaristus@gmail.com>
2026-06-13 06:00:29 +0000

    Force service worker cache clear v3

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = 'skillup-v2';
+const CACHE_NAME = 'skillup-v3';
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => 
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
